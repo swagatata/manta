20464c4905c1ea334e909904f6744e812b9470d7
Swagat Konchada <konchada@fb.com>
2021-12-12 00:20:53 +0000

    also test lenght of the encoding

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,21 @@ mod tests {
     use rstest::*;
 
     #[rstest]
-    #[case("")]
-    #[case("s")]
-    #[case("*")]
-    #[case("swag")]
-    #[case("aabaa")]
-    #[case("ababcbababaa")]
-    #[case("ababcbababacbaaa")]
-    fn test_encode_decode(#[case] s: &str) {
+    #[case("", 0)]
+    #[case("s", 1)]
+    #[case("*", 1)]
+    #[case("swag", 4)]
+    #[case("aabaa", 3)]
+    #[case("ababcbababaa", 5)]
+    #[case("ababcbababacbaaa", 7)]
+    fn test_encode_decode(#[case] s: &str, #[case] encoded_text_size: usize) {
         let plain_text = s.to_string();
         let naive_matcher = super::compression::matchers::NaiveMatcher {};
         let compressor = super::compression::Compressor{
             matcher: Box::new(naive_matcher),
         }; 
         let encoded_text = compressor.compress(&plain_text);
+        assert_eq!(encoded_text.len(), encoded_text_size);
         println!("printing encoded text");
         for entry in &encoded_text {
             println!("({}, {}, {})", entry.0, entry.1, entry.2);
