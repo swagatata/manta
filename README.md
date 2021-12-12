a62bb78225facc553ee6282f9cd36d9836745007
Swagat Konchada <konchada@fb.com>
2021-12-12 18:56:36 +0000

    add bench tests

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Building a compression/decompression program
 
 Run all test cases : `cargo test`
+Run all profiling tests : `cargo bench`
---
+++ benches/large-input.rs
@@ -0,0 +1,50 @@
+#![feature(test)]
+extern crate test;
+
+// #[path = "../src/compression.rs"] // Here
+// #[path = "../src/compression/matchers.rs"] // Here
+// mod compression;
+
+#[cfg(test)]
+mod tests {
+    use test::{black_box, Bencher};
+    use manta::compression;
+
+    fn encode_decode_helper(
+        s: &String,
+        search_buffer_size: usize,
+        lookahead_size: usize,
+    ) -> String {
+        let naive_matcher = compression::matchers::NaiveMatcher {
+            search_buffer_size: search_buffer_size,
+            lookahead_buffer_size: lookahead_size,
+        };
+        let compressor = compression::Compressor {
+            matcher: Box::new(naive_matcher),
+        };
+        let encoded_text = compressor.compress(s);
+        println!("printing encoded text");
+        for entry in &encoded_text {
+            println!("({}, {}, {})", entry.0, entry.1, entry.2);
+        }
+        compressor.decompress(encoded_text)
+    }
+
+    #[bench]
+    fn bench_hundred_thousand(b: &mut Bencher) {
+        b.iter(|| {
+            let plain_text = "s".repeat(100000);
+            let decoded_text = encode_decode_helper(&plain_text, 0, 0);
+            assert_eq!(plain_text, decoded_text);
+        });
+    }
+
+    #[bench]
+    fn bench_million(b: &mut Bencher) {
+        b.iter(|| {
+            let plain_text = "s".repeat(1000000);
+            let decoded_text = encode_decode_helper(&plain_text, 0, 0);
+            assert_eq!(plain_text, decoded_text);
+        });
+    }
+}
